384852c3bdebd3e2ccb066d1fd6bae542595d4d7
Cristoper Yarleque <caypsaturno@gmail.com>
2023-04-08 20:48:08 -0500

    Chore: update

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,6 @@
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
+ENV['RAILS_MASTER_KEY'] = 'f02981781406ae51249b2532e4cdab25'
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
